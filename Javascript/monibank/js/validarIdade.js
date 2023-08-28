1497687cbcb9eafd0c71a7d259fbe73b0bbf76d7
valdecyjr <valdecysjunior@gmail.com>
2023-08-28 20:01:14 -0300

    monibank projeto finalizado

---
+++ Javascript/monibank/js/validarIdade.js
@@ -0,0 +1,14 @@
+export default function validarIdade(campo){
+    const dataNascimento = new Date(campo.value);
+
+    if (!validaIdade(dataNascimento)){
+        campo.setCustomValidity('O usuário não é maior de idade');
+    }
+}
+
+function validaIdade(data){
+    const dataAtual = new Date();
+    const dataMaiorDeIdade = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+
+    return dataAtual >= dataMaiorDeIdade;
+}
